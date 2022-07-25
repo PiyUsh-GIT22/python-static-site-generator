c3b2d8183b8163d507a1dfd6b9670da729b40e1e
PiyUsh-GIT22 <piyush.pandey@stl.tech>
2022-07-25 20:41:17 +0530

    First Commit

--- ssg/content.py
+++ ssg/content.py
@@ -5,33 +5,33 @@ from collections.abc import Mapping
 class Contet(Mapping):
     __delimeter = r"^(?:-|\+){3}\s*$"
     __regex = re.compile(__delimiter,re.MULTILINE)
-     @classmethod
-     def load(cls, string):
+    @classmethod
+    def load(cls, string):
         _, fm, content = cls.__regex.split(string, 2)
         metadata = load(fm, Loader=FullLoader)
 
         return cls(metadata, content)
-     def __init__(self,metadata,content):
+    def __init__(self,metadata,content):
           self.data = metadata
           self.data["content"] = content
-     @property
-     def body(self):
+    @property
+    def body(self):
         return self.data["content"]
-     @property
-     def type(self):
+    @property
+    def type(self):
         return self.data["type"] if "type" in self.data else None
 
-     @type.setter
-     def type(self, type):
+    @type.setter
+    def type(self, type):
         self.data["type"] = type
-     def __getitem__(self,key):
+    def __getitem__(self,key):
          return self.data[key]
-     def __iter__(self):
+    def __iter__(self):
          self.data.__iter__()
 
-     def __len__(self):
+    def __len__(self):
          return len(self.data)
-     def __repr__(self):
+    def __repr__(self):
          data = {}
          for key, value in self.data.items():
             if key != "content":
