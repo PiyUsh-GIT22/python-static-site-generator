11547e218f8f96c96de0ff8303ce39f4d87757e0
PiyUsh-GIT22 <piyush.pandey@stl.tech>
2022-07-25 18:46:41 +0530

    First Commit

--- ssg.py
+++ ssg.py
@@ -1,18 +1,12 @@
 import typer
 
-import ssg.parsers
+
 
 from ssg.site import Site
 def main(source="content", dest="dist"):
     config = {
         "source": source,
-        "dest": dest,
-        "parsers": [
-            ssg.parsers.MarkdownParser(),
-            ssg.parsers.ReStructuredTextParser(),
-            ssg.parsers.ResourceParser(),
-        ],
-    }
+        "dest": dest,}
 Site(**config).build()
 
 
