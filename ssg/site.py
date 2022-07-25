3d805de9f568e39265b105db4b48d86bc63bbe8e
PiyUsh-GIT22 <piyush.pandey@stl.tech>
2022-07-25 22:10:49 +0530

    First Commit

--- ssg/site.py
+++ ssg/site.py
@@ -26,4 +26,7 @@ class Site:
         if parser is not None:
             parser.parse(path,self.source,self.dest)
         else:
-            print("Not Implemented")
+            self.error("No parser for the {} extension, file skipped!".format(path.suffix))
+    @staticmethod
+    def error(message, end="\n"):
+        sys.stderr.write("\x1b[1;31m" + message.strip() + "\x1b[0m" + end)
